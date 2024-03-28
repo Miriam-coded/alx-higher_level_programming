14641de3e6beb3ffd2960505f0d9c0d5bc894b2e
Miriam-coded <miriamwmukuhi@gmail.com>
2024-03-28 05:14:48 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    add = 0
+    n = len(argv)
+
+    for i in range(1, n):
+        add += int(argv[i])
+
+    print(add)
