b374e04ef97eaa870436571969207c76d770520b
Miriam-coded <miriamwmukuhi@gmail.com>
2024-04-14 07:42:58 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_score  = max(a_dictionary, key=a_dictionary.get)
-    return max_score
+    else:
+        max_score = max(a_dictionary)
+        return max_score
